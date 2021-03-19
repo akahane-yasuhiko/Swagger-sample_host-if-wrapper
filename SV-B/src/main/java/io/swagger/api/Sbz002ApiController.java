213279faa2f622a8c63353f6ef5da7baab3127a2
akahane-yasuhiko <akahane.yasuhiko@gmail.com>
2021-03-19 22:27:35 +0900

    自作サービスを呼び出すように改修

--- SV-B/src/main/java/io/swagger/api/Sbz002ApiController.java
+++ SV-B/src/main/java/io/swagger/api/Sbz002ApiController.java
@@ -1,37 +1,21 @@
 package io.swagger.api;
 
-import io.swagger.model.Sbz002bReq;
-import io.swagger.model.Sbz002bRes;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.Sbz002bReq;
+import io.swagger.model.Sbz002bRes;
+import io.swagger.service.Sbz002Service;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-19T21:32:23.879104+09:00[Asia/Tokyo]")
 @RestController
@@ -43,6 +27,9 @@ public class Sbz002ApiController implements Sbz002Api {
 
     private final HttpServletRequest request;
 
+    @Autowired
+    private Sbz002Service service;
+
     @org.springframework.beans.factory.annotation.Autowired
     public Sbz002ApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
@@ -50,17 +37,8 @@ public class Sbz002ApiController implements Sbz002Api {
     }
 
     public ResponseEntity<Sbz002bRes> sbz002(@Parameter(in = ParameterIn.DEFAULT, description = "description here", required=true, schema=@Schema()) @Valid @RequestBody Sbz002bReq body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Sbz002bRes>(objectMapper.readValue("{\n  \"statusCode\" : \"statusCode\"\n}", Sbz002bRes.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Sbz002bRes>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<Sbz002bRes>(HttpStatus.NOT_IMPLEMENTED);
+        Sbz002bRes res = service.execute(body);
+        return new ResponseEntity<Sbz002bRes>(res, HttpStatus.OK);
     }
 
 }
